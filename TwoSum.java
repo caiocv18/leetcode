9b693ae8b2d577516226a622b0c2e8ac3d20ba37
Caio Vinícius <caioviniciuscv18@gmail.com>
2025-06-02 22:22:44 -0300

    Optimize TwoSum solution using a HashMap for improved performance

--- TwoSum.java
+++ TwoSum.java
@@ -1,14 +1,21 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+            int complement = target - nums[i];
+            
+            if (numToIndex.containsKey(complement)) {
+                return new int[] { numToIndex.get(complement), i };
             }
+            
+            numToIndex.put(nums[i], i);
         }
+        
         return null;
     }
 }
